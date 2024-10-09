888ffe69ae7b5819ad49b36a1a5cad5a28b402af
kaushal1014 <kaushal.patil163@gmail.com>
2024-10-09 01:57:56 -0700

    Updated schema

--- server/models/PhdScholar.js
+++ server/models/PhdScholar.js
@@ -23,27 +23,67 @@ const phdScholarSchema = new mongoose.Schema({
     srn: String,
     modeOfProgram: String,  // Full Time/Part Time - Internal/External
   },
-  researchDetails: {
-    researchSupervisor: String,
-    researchCoSupervisor: String,
-    doctoralCommittee: {
+  researchSupervisor: String,
+  researchCoSupervisor: String,
+  doctoralCommittee: {
       member1: String,
       member2: String,
       member3: String,
       member4: String
-    },
   },
+/*
   courseWork: [{
-    course: String,
     subjectCode: String,
     subjectName: String,
     subjectGrade: String,
     status: String,
     eligibilityDate: Date
   }],
+*/
+courseWork1: {
+  subjectCode: String,
+  subjectName: String,
+  subjectGrade: String,
+  status: String,
+  eligibilityDate: Date
+},
+  courseWork2: {
+    subjectCode: String,
+    subjectName: String,
+    subjectGrade: String,
+    status: String,
+    eligibilityDate: Date
+  },
+  courseWork3: {
+    subjectCode: String,
+    subjectName: String,
+    subjectGrade: String,
+    status: String,
+    eligibilityDate: Date
+  },
+  courseWork4: {
+    subjectCode: String,
+    subjectName: String,
+    subjectGrade: String,
+    status: String,
+    eligibilityDate: Date
+  },
+  
   phdMilestones: {
-    courseworkCompletionDate: Date,
-    dcMeetings: [Date],  // Dates for Doctoral Committee Meetings
+    courseworkCompletionDate:{
+      coursework1:Date,
+      coursework2:Date,
+      coursework3:Date,
+      coursework4:Date,
+    },
+    dcMeetings:{
+      DCM:[{
+        scheduledDate:Date, 
+        actualDate:Date
+      }]
+    },  // Dates for Doctoral Committee Meetings
+    
+    
     comprehensiveExamDate: Date,
     proposalDefenseDate: Date,
     openSeminarDate1: Date,
@@ -51,7 +91,7 @@ const phdScholarSchema = new mongoose.Schema({
     synopsisSubmissionDate: Date,
     thesisSubmissionDate: Date,
     thesisDefenseDate: Date,
-    awardDate: Date
+    awardOfDegreeDate: Date
   },
   publications: {
     journals: [{
