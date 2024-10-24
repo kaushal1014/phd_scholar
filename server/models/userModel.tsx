fbaf161b8ac8370176552b0819318105fdbb8ee7
kaushal1014 <kaushal.patil163@gmail.com>
2024-10-24 05:31:09 -0700

    removed username

--- server/models/userModel.tsx
+++ server/models/userModel.tsx
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: [true, "Please provide a username"],
-        unique: true,
-    },
     email: {
         type: String,
         required: [true, "Please provide a email"],
