63a31c971836737e4034b8b1bec7bb10bc00bda7
Kaushal Patil <kaushal.patil163@gmail.com>
2024-10-31 22:07:54 +0530

    Added contet

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,54 @@
-'use client';
-import React from 'react'
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-function page() {
-    return ( 
-        <div>
-            <h1>hi</h1>
+export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  const showSession = () => {
+    if (status === "authenticated") {
+      return (
+        <button
+          className="border border-solid border-black rounded px-4 py-2 hover:bg-gray-200"
+          onClick={() => {
+            signOut({ redirect: false }).then(() => {
+              router.push("/");
+            });
+          }}
+        >
+          Sign Out
+        </button>
+      );
+    } else if (status === "loading") {
+      return (
+        <div className="flex items-center justify-center min-h-screen">
+          <svg
+            className="animate-spin h-5 w-5 mr-2 text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeWidth="4"/>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 1 0 16 0A8 8 0 0 0 4 12z"/></svg>
+          <span className="text-gray-500 text-sm">Loading...</span>
         </div>
-    );
-}
+      );
+    } else {
+      return (
+        <Link
+          href="/login"
+          className="border border-solid border-black rounded px-4 py-2 hover:bg-gray-200"
+        >
+          Sign In
+        </Link>
+      );
+    }
+  };
 
-export default page;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-xl">Home</h1>
+      {showSession()}
+    </main>
+  );
+}
