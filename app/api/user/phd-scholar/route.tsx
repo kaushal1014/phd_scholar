52f09399e53dca128880dd7c51938555cbe099c0
Kaushal Patil <kaushal.patil163@gmail.com>
2024-12-22 17:02:41 +0530

    get api for user data

---
+++ app/api/user/phd-scholar/route.tsx
@@ -0,0 +1,24 @@
+import { NextResponse, NextRequest } from 'next/server';
+import connectDB from '@/server/db';
+import User from '@/server/models/userModel';
+import { getToken } from 'next-auth/jwt';
+
+connectDB();
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    if (!token) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const user = await User.findById(token.id).populate('phdScholar');
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  }
+}
