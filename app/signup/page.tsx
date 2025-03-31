d9f38c98e0c124537d57273dd70cbccaba5fb676
Kaushal Patil <kaushal.patil163@gmail.com>
2025-03-31 17:27:14 +0530

    Only pesu emails can login

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -403,7 +403,7 @@ const PhDScholarForm: React.FC = () => {
               id="email"
               name="email"
               type="email"
-              placeholder="john@example.com"
+              placeholder="example@pesu.pes.edu"
               onChange={handleChange}
                 
               className="w-full px-3 py-2 border-2 border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent dark:bg-gray-700 dark:text-gray-100"
